ce96366040a994f569536b14bb426cd03a753aad
Anton Belodedenko <anton.belodedenko@gmail.com>
2015-03-18 11:25:48 +0000

    Update build.sh
    Fixed iptables FORWARD chain rules

--- build.sh
+++ build.sh
@@ -20,6 +20,22 @@ clientip=$(echo $SSH_CONNECTION | awk '{print $1}')
 # get the current date
 date=$(/bin/date +'%Y%m%d')
 
+# configure iptables
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p icmp -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
+iptables -A INPUT -s $clientip/32 -p tcp -m tcp --dport 80 -j ACCEPT
+iptables -A INPUT -s $clientip/32 -p tcp -m tcp --dport 443 -j ACCEPT
+iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
+iptables -A FORWARD -j REJECT --reject-with icmp-host-prohibited
+iptables -A DOCKER -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A DOCKER -p icmp -j ACCEPT
+iptables -A DOCKER -s $clientip/32 -p udp -m udp --dport 53 -j ACCEPT
+iptables -A DOCKER -j REJECT --reject-with icmp-host-prohibited
+iptables-save > /etc/iptables.rules
+grep -q 'pre-up iptables-restore < /etc/iptables.rules' /etc/network/interfaces || printf '\tpre-up iptables-restore < /etc/iptables.rules\n'  >> /etc/network/interfaces
+
 echo "Updating db.override with ipaddr"=$ipaddr "and date="$date
 $(which sed) -i "s/127.0.0.1/${ipaddr}/g" data/db.override
 $(which sed) -i "s/YYYYMMDD/${date}/g" data/db.override
@@ -40,23 +56,6 @@ echo "GET /" | $(which openssl) s_client -servername netflix.com -connect $ipadd
 
 # configure upstart
 cp init/* /etc/init
-
-# configure iptables
-iptables -I INPUT 1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -I INPUT 2 -p icmp -j ACCEPT
-iptables -I INPUT 3 -i lo -j ACCEPT
-iptables -I INPUT 4 -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
-iptables -I INPUT 5 -s $clientip/32 -p tcp -m tcp --dport 80 -j ACCEPT
-iptables -I INPUT 6 -s $clientip/32 -p tcp -m tcp --dport 443 -j ACCEPT
-iptables -I INPUT 7 -j REJECT --reject-with icmp-host-prohibited
-iptables -D FORWARD 1
-iptables -I FORWARD 4 -j REJECT --reject-with icmp-host-prohibited
-iptables -I DOCKER 1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -I DOCKER 2 -p icmp -j ACCEPT
-iptables -I DOCKER 3 -s $clientip/32 -p udp -m udp --dport 53 -j ACCEPT
-iptables -I DOCKER 4 -j REJECT --reject-with icmp-host-prohibited
-iptables-save > /etc/iptables.rules
-grep -q 'pre-up iptables-restore < /etc/iptables.rules' /etc/network/interfaces || printf '\tpre-up iptables-restore < /etc/iptables.rules\n'  >> /etc/network/interfaces
 
 # change back to original directory
 popd
