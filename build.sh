644a922a8e51f67b0ac6fe972c5e64be95108c21
Anton Belodedenko <anton.belodedenko@gmail.com>
2015-06-19 15:56:51 -0400

    Added DNS recursion option

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ set -e
 
 # change to working directory
 root="/opt/netflix-proxy"
-pushd $root
 
 # obtain the interface with the default gateway say
 int=$(ip route | grep default | awk '{print $5}')
@@ -21,6 +20,52 @@ clientip=$(echo $SSH_CONNECTION | awk '{print $1}')
 
 # get the current date
 date=$(/bin/date +'%Y%m%d')
+
+# display usage
+usage() {
+	echo "Usage: $0 [-r 0|1] [-b 0|1]" 1>&2; \
+	printf "\t-r\tenable (1) or disable (0) DNS recursion (default: 0)\n"; \
+        printf "\t-b\tgrab docker images from repository (0) or build locally (1) (default: 0)\n"; \
+	exit 1;
+}
+
+# process options
+while getopts ":r:b:" o; do
+    case "${o}" in
+        r)
+            r=${OPTARG}
+            ((r == 0|| r == 1)) || usage
+            ;;
+        b)
+            b=${OPTARG}
+            ((b == 0|| b == 1)) || usage
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [[ -z "${r}" ]]; then
+	r=0
+fi
+
+if [[ -z "${b}" ]]; then
+        b=0
+fi
+
+# prepare BIND config
+if [[ ${r} == 0 ]]; then
+	printf "disabling DNS recursion...\n"
+	printf "\t\tallow-recursion { none; };\n\t\trecursion no;\n\t\tadditional-from-auth no;\n\t\tadditional-from-cache no;\n" > ${root}/docker-bind/named.recursion.conf
+else
+	printf "WARNING: enabling DNS recursion...\n"
+	printf "\t\tallow-recursion { trusted; };\n\t\trecursion yes;\n\t\tadditional-from-auth yes;\n\t\tadditional-from-cache yes;\n" > ${root}/docker-bind/named.recursion.conf	
+fi
+
+# switch to working directory
+pushd ${root}
 
 # configure iptables
 sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
@@ -43,9 +88,19 @@ echo "Updating db.override with ipaddr"=$extip "and date="$date
 sudo $(which sed) -i "s/127.0.0.1/${extip}/g" data/db.override
 sudo $(which sed) -i "s/YYYYMMDD/${date}/g" data/db.override
 
-echo "Starting Docker containers"
-sudo $(which docker) run --name bind -d -v /opt/netflix-proxy/data:/data -p 53:53/udp -t ab77/bind
-sudo $(which docker) run --name sniproxy -d -v /opt/netflix-proxy/data:/data --net=host -t ab77/sniproxy
+if [[ "${b}" == "1" ]]; then
+	echo "Building docker containers"
+	$(which docker) build -t bind docker-bind
+	$(which docker) build -t sniproxy docker-sniproxy
+
+	echo "Starting Docker containers (local)"
+	sudo $(which docker) run --name bind -d -v ${root}/data:/data -p 53:53/udp -t bind
+	sudo $(which docker) run --name sniproxy -d -v ${root}/data:/data --net=host -t sniproxy
+else
+	echo "Starting Docker containersi (from repository)"
+	sudo $(which docker) run --name bind -d -v ${root}/data:/data -p 53:53/udp -t ab77/bind
+	sudo $(which docker) run --name sniproxy -d -v ${root}/data:/data --net=host -t ab77/sniproxy
+fi
 
 echo "Testing DNS"
 $(which dig) netflix.com @$ipaddr
