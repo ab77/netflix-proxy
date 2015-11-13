e1388fec811e073644f4b8f1fca0504efcbcad26
Anton Belodedenko <anton.belodedenko@gmail.com>
2015-11-13 15:28:01 +0000

    Added flags to support Microsoft Azure deploys

--- build.sh
+++ build.sh
@@ -26,15 +26,17 @@ date=$(/bin/date +'%Y%m%d')
 
 # display usage
 usage() {
-	echo "Usage: $0 [-r 0|1] [-b 0|1] [-c <ip>]" 1>&2; \
+	echo "Usage: $0 [-r 0|1] [-b 0|1] [-c <ip>] [-i 0|1] [-d 0|1]" 1>&2; \
 	printf "\t-r\tenable (1) or disable (0) DNS recursion (default: 1)\n"; \
 	printf "\t-b\tgrab docker images from repository (0) or build locally (1) (default: 0)\n"; \
 	printf "\t-c\tspecify client-ip instead of being taken from ssh_connection\n"; \
+	printf "\t-i\tskip iptables steps\n"; \
+	printf "\t-d\tskip Docker steps\n"; \
 	exit 1;
 }
 
 # process options
-while getopts ":r:b:c:" o; do
+while getopts ":r:b:c:i:d:" o; do
 	case "${o}" in
 		r)
 			r=${OPTARG}
@@ -46,6 +48,14 @@ while getopts ":r:b:c:" o; do
 			;;
 		c)
 			c=${OPTARG}
+			;;
+		i)
+			i=${OPTARG}
+			((i == 0|| i == 1)) || usage
+			;;
+		d)
+			d=${OPTARG}
+			((d == 0|| d == 1)) || usage
 			;;
 		*)
 			usage
@@ -66,8 +76,16 @@ if [[ -n "${c}" ]]; then
 	clientip="${c}"
 fi
 
+if [[ -z "${i}" ]]; then
+	i=0
+fi
+
+if [[ -z "${d}" ]]; then
+	d=0
+fi
+
 # diagnostics info
-echo "clientip="$clientip "ipaddr="$ipaddr "extip"=$extip
+echo "clientip="$clientip "ipaddr="$ipaddr "extip"=$extip "-r"=${r} "-b"=${b} "-i"=${i} "-d"=${d}
 
 # prepare BIND config
 if [[ ${r} == 0 ]]; then
@@ -81,44 +99,48 @@ fi
 # switch to working directory
 pushd ${root}
 
-# configure iptables
-sudo iptables -N FRIENDS
-sudo iptables -A FRIENDS -s $clientip/32 -j ACCEPT
-sudo iptables -A FRIENDS -j DROP
-sudo iptables -N ALLOW
-sudo iptables -A INPUT -j ALLOW
-sudo iptables -A FORWARD -j ALLOW
-sudo iptables -A DOCKER -j ALLOW
-sudo iptables -A ALLOW -p icmp -j ACCEPT
-sudo iptables -A ALLOW -i lo -j ACCEPT
-sudo iptables -A ALLOW -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
-sudo iptables -A ALLOW -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A ALLOW -p tcp -m tcp --dport 80 -j FRIENDS
-sudo iptables -A ALLOW -p tcp -m tcp --dport 443 -j FRIENDS
-sudo iptables -A ALLOW -p tcp -m tcp --dport 43867 -j FRIENDS
-sudo iptables -A ALLOW -p udp -m udp --dport 53 -j FRIENDS
-sudo iptables -A ALLOW -j REJECT --reject-with icmp-host-prohibited
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
-sudo apt-get -y install iptables-persistent
-printf "\tpre-up service iptables-persistent reload\n" | sudo tee /etc/network/interfaces
+if [[ ${i} == 0 ]]; then
+	# configure iptables
+	sudo iptables -N FRIENDS
+	sudo iptables -A FRIENDS -s $clientip/32 -j ACCEPT
+	sudo iptables -A FRIENDS -j DROP
+	sudo iptables -N ALLOW
+	sudo iptables -A INPUT -j ALLOW
+	sudo iptables -A FORWARD -j ALLOW
+	sudo iptables -A DOCKER -j ALLOW
+	sudo iptables -A ALLOW -p icmp -j ACCEPT
+	sudo iptables -A ALLOW -i lo -j ACCEPT
+	sudo iptables -A ALLOW -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
+	sudo iptables -A ALLOW -m state --state RELATED,ESTABLISHED -j ACCEPT
+	sudo iptables -A ALLOW -p tcp -m tcp --dport 80 -j FRIENDS
+	sudo iptables -A ALLOW -p tcp -m tcp --dport 443 -j FRIENDS
+	sudo iptables -A ALLOW -p tcp -m tcp --dport 43867 -j FRIENDS
+	sudo iptables -A ALLOW -p udp -m udp --dport 53 -j FRIENDS
+	sudo iptables -A ALLOW -j REJECT --reject-with icmp-host-prohibited
+	echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+	echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+	sudo apt-get -y install iptables-persistent
+	printf "\tpre-up service iptables-persistent reload\n" | sudo tee /etc/network/interfaces
+fi
 
 echo "Updating db.override with ipaddr"=$extip "and date="$date
 sudo $(which sed) -i "s/127.0.0.1/${extip}/g" data/db.override
 sudo $(which sed) -i "s/YYYYMMDD/${date}/g" data/db.override
 
-if [[ "${b}" == "1" ]]; then
-	echo "Building docker containers"
-	sudo $(which docker) build -t bind docker-bind
-	sudo $(which docker) build -t sniproxy docker-sniproxy
-
-	echo "Starting Docker containers (local)"
-	sudo $(which docker) run --name bind -d -v ${root}/data:/data -p 53:53/udp -t bind
-	sudo $(which docker) run --name sniproxy -d -v ${root}/data:/data --net=host -t sniproxy
-else
-	echo "Starting Docker containers (from repository)"
-	sudo $(which docker) run --name bind -d -v ${root}/data:/data -p 53:53/udp -t ab77/bind
-	sudo $(which docker) run --name sniproxy -d -v ${root}/data:/data --net=host -t ab77/sniproxy
+if [[ ${d} == 0 ]]; then
+	if [[ "${b}" == "1" ]]; then
+		echo "Building docker containers"
+		sudo $(which docker) build -t bind docker-bind
+		sudo $(which docker) build -t sniproxy docker-sniproxy
+	
+		echo "Starting Docker containers (local)"
+		sudo $(which docker) run --name bind -d -v ${root}/data:/data -p 53:53/udp -t bind
+		sudo $(which docker) run --name sniproxy -d -v ${root}/data:/data --net=host -t sniproxy
+	else
+		echo "Starting Docker containers (from repository)"
+		sudo $(which docker) run --name bind -d -v ${root}/data:/data -p 53:53/udp -t ab77/bind
+		sudo $(which docker) run --name sniproxy -d -v ${root}/data:/data --net=host -t ab77/sniproxy
+	fi
 fi
 
 echo "Testing DNS"
