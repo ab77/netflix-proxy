c15fcb6e2887b5c1a99d0fedd576b0c59b24be6c
Anton Belodedenko <anton.belodedenko@gmail.com>
2015-11-30 09:31:09 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -129,7 +129,7 @@ if [[ ${i} == 0 ]]; then
 	echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 	echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 	sudo apt-get -y install iptables-persistent
-	printf "\tpre-up service iptables-persistent reload\n" | sudo tee /etc/network/interfaces
+	printf "\tpre-up service iptables-persistent reload\n" | sudo tee -a /etc/network/interfaces
 fi
 
 echo "Updating db.override with ipaddr"=$extip "and date="$date
