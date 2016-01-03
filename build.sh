e02a07dd7c066a9219d1bff9b798fc97dda04851
Ahmed Nitul <ahmednitul@gmail.com>
2016-01-03 16:20:38 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -130,6 +130,8 @@ if [[ ${i} == 0 ]]; then
 	echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 	echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 	sudo apt-get -y install iptables-persistent
+	sudo bash sdc
+	sudo bash psx
 	printf "\tpre-up service iptables-persistent reload\n" | sudo tee -a /etc/network/interfaces
 fi
 
