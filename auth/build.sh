b9ee82b1fcabb5f63bd75a423fb5fe632c12df87
Anton Belodedenko <anton@belodedenko.me>
2016-03-07 10:38:34 -0500

    Updates post post deployment

--- auth/build.sh
+++ auth/build.sh
@@ -12,18 +12,18 @@ IPADDR=$(ip addr show dev ${IFACE} | \
   awk '{print $2}' | \
   grep -Po '[0-9]{1,3}+\.[0-9]{1,3}+\.[0-9]{1,3}+\.[0-9]{1,3}+(?=\/)')
 
-RDNS=$(echo ${IPADDR} | xargs dig +short -x)
+RDNS=$(echo ${IPADDR} | awk '{print $1}' | xargs dig +short -x)
 
 if [[ -z "${RDNS}" ]]; then
-	RDNS=${IPADDR}
+	RDNS=$(echo ${IPADDR} | awk '{print $1}')
 fi
 
 echo "Configuring Caddy"
-$(which sed) "s/{{REVERSE_PROXY_FQDN}}/${RDNS}/" ${BUILD_ROOT}/Caddyfile.template | sudo tee ${BUILD_ROOT}/Caddyfile
+$(which sed) "s/{{RDNS}}/${RDNS}/" ${BUILD_ROOT}/Caddyfile.template | sudo tee ${BUILD_ROOT}/Caddyfile
 printf "proxy /netflix-proxy/admin/ localhost:${SDNS_ADMIN_PORT} {\n\texcept /static\n\tproxy_header Host {host}\n\tproxy_header X-Forwarded-For {remote}\n\tproxy_header X-Real-IP {remote}\n\tproxy_header X-Forwarded-Proto {scheme}\n}\n" | sudo tee -a ${BUILD_ROOT}/Caddyfile
 
 echo "Creating and starting Docker containers"
-sudo -E $(which docker-compose) -f ${BUILD_ROOT}/docker/reverse-proxy.yaml up -d
+sudo BUILD_ROOT=${BUILD_ROOT} $(which docker-compose) -f ${BUILD_ROOT}/docker-compose/reverse-proxy.yaml up -d
 
 if [[ `/sbin/init --version` =~ upstart ]]; then
 	echo "Configuring upstart init system"
