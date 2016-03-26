fa547edfb8efc2973db1377896482a25d435e7dc
root <root@beasthost1.belodedenko.me>
2016-03-26 07:36:50 -0400

    Added ip address random selection

--- docker-sniproxy/sniproxy.sh
+++ docker-sniproxy/sniproxy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# globals
+RANDOM_IPS=10
+
+# import common functions
+CDW=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+. ${CDW}/functions
+
 # set environment from linked container information
 RESOLVER_IP=$(grep caching-resolver /etc/hosts | awk '{print $1}')
 
@@ -10,6 +17,9 @@ fi
 # update sniproxy config
 printf "Setting sniproxy resolver to ${RESOLVER_IP}\n"
 sed -i -r "s/nameserver ([0-9]{1,3}+\.[0-9]{1,3}+\.[0-9]{1,3}+\.[0-9]{1,3})/nameserver ${RESOLVER_IP}/" /data/conf/sniproxy.conf
+
+# add source IP loadbalancing
+add_source_ipv6_lb ${RANDOM_IPS}
 
 # launch sniproxy
 /usr/sbin/sniproxy -c /data/conf/sniproxy.conf -f
