d9202f678d005f8f60d24b18b5c5c34614416cf9
Anton Belodedenko <nton@belodedenko.me>
2016-04-07 17:04:15 +0100

    Updated gh-pages push

--- tests/gh-pages-push.sh
+++ tests/gh-pages-push.sh
@@ -17,6 +17,7 @@ if [ "${TRAVIS_REPO_SLUG}" == "${GH_REPO}" ] && [ "${TRAVIS_PULL_REQUEST}" == "f
   cd gh-pages
   rm -rf artifacts && mkdir -p artifacts
   cp -Rf ${HOME}/artifacts artifacts
+  ls -la artifacts
   git add -f .
   git commit -m "Auto-push from Travis build ${TRAVIS_BUILD_NUMBER}"
   git push -fq origin gh-pages > /dev/null
