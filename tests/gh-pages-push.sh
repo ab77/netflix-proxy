400cec6cc4a8c1374a72e9a1ad26e6cf14aa5bd5
Anton Belodedenko <nton@belodedenko.me>
2016-04-07 16:07:20 +0100

    Added artifacts push

---
+++ tests/gh-pages-push.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+BRANCH=master
+
+if [ "${TRAVIS_REPO_SLUG}" == "${GH_REPO}" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "${BRANCH}" ]; then
+
+  printf "Publishing artifacts for ${TRAVIS_BUILD_NUMBER} to ${TRAVIS_REPO_SLUG}:${TRAVIS_BRANCH}...\n"
+
+  cp -R tests/artifacts/*.png ${HOME}/artifacts
+
+  cd ${HOME}
+  git config --global user.email "${GH_EMAIL}"
+  git config --global user.name "${GH_NAME}"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${GH_REPO} gh-pages > /dev/null
+
+  cd gh-pages
+  rm -rf artifacts && mkdir -p artifacts
+  cp -Rf ${HOME}/artifacts artifacts
+  git add -f .
+  git commit -m "Artifacts from ${TRAVIS_BUILD_NUMBER} auto-pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  printf "Published artifacts to gh-pages.\n"
+fi
