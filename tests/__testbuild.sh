9cf83c95d659677c8ad41578be45b1a78017bef0
Anton Belodedenko <nton@belodedenko.me>
2016-04-07 13:30:48 +0100

    Added manual build script

---
+++ tests/__testbuild.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+token=${1}
+user=ab77
+repository=netflix-proxy
+branch=master
+
+if [[ -z ${1} ]]; then
+    printf "Usage: ${0} <travis-ci-token>\n" 
+    exit
+fi
+
+body='{
+"request": {
+  "branch":"${branch}"
+}}'
+
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token ${token}" \
+  -d "${body}" \
+  https://api.travis-ci.org/repo/${user}%2F${repository}/requests
