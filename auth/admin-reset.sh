7a9abe99bf6a4cad9e70d873101d704598b7004d
Anton Belodedenko <anton@belodedenko.me>
2016-04-25 03:47:28 -0400

    Fixed password reset script

--- auth/admin-reset.sh
+++ auth/admin-reset.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# UN-TESTED
+
 # globals
 BUILD_ROOT=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 SQLITE_DB=${BUILD_ROOT}/db/auth.db
@@ -8,5 +8,6 @@ read -sp "Please enter a new admin password: " PASSWORD && printf "\n"
 
     pushd ${BUILD_ROOT} && \
       export HASH=`${BUILD_ROOT}/pbkdf2_sha256_hash.py ${PASSWORD} | awk '{print $2}'` && \
-      sqlite3 ${SQLITE_DB} "UPDATE USERS SET password='yourpasswordhashhere' WHERE Id=1;" && \
-      popd
+      sqlite3 ${SQLITE_DB} "UPDATE USERS SET password='${HASH}' WHERE id=1;" && \
+      popd && \
+      printf "rc=$?\n"
