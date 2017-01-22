706a6d22dc9130d500b38790302454cba2eeab66
Patrice Brend'amour <patrice@brendamour.net>
2017-01-22 23:35:22 +0100

    WIP: added DDNS script and table. Allows automatically updating IPs

---
+++ scripts/ddns_updates.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# ddns_updater.sh: Checks DDNS sites and updates the IPs if needed.
+# author: patrice@brendamour.net
+
+# Note, this script assumes Ubuntu or Debian Linux and it will most likely fail on any other distribution.
+
+# bomb on any error
+set -e
+
+CDW=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+SQLITE_DB=${CDW}/../auth/db/auth.db
+
+. ${CDW}/functions
+
+LIST=`sudo $(which sqlite3) ${SQLITE_DB} "SELECT domain,last_ipaddr FROM DDNS"`
+for ROW in $LIST; do
+
+DOMAIN=`echo $ROW | awk '{split($0,a,"|"); print a[1]}'`
+OLDIP=`echo $ROW | awk '{split($0,a,"|"); print a[2]}'`
+NEWIP=`dig +short $DOMAIN`
+
+iptables -t nat -D PREROUTING -s $OLDIP/32 -i %s -j ACCEPT -v && iptables-save > /etc/iptables/rules.v4 || iptables-save > /etc/iptables.rules
+iptables -t nat -I PREROUTING -s $NEWIP/32 -i %s -j ACCEPT -v && iptables-save > /etc/iptables/rules.v4 || iptables-save > /etc/iptables.rules
+
+#UPDATE database
+sudo $(which sqlite3) ${SQLITE_DB} "UPDATE DDNS SET last_ipaddr = '${NEWIP}' WHERE domain = '${DOMAIN}'"
+
+
+done 
