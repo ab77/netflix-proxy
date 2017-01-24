d46af6500e3559c2c5de5110e1165c7d0db92e4e
Patrice Brend'amour <patrice@brendamour.net>
2017-01-25 00:03:19 +0100

    added check for changes in IP (no need to remove & add the same IP over and over)

--- scripts/ddns_updates.sh
+++ scripts/ddns_updates.sh
@@ -9,23 +9,29 @@
 set -e
 
 CDW=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+. ${CDW}/functions
 
 SQLITE_DB=${CDW}/../auth/db/auth.db
 
-. ${CDW}/functions
+# obtain the interface with the default gateway
+IFACE=$(get_iface)
 
 LIST=`sudo $(which sqlite3) ${SQLITE_DB} "SELECT domain,last_ipaddr FROM DDNS"`
 for ROW in $LIST; do
+        DOMAIN=`echo $ROW | awk '{split($0,a,"|"); print a[1]}'`
+        OLDIP=`echo $ROW | awk '{split($0,a,"|"); print a[2]}'`
+        NEWIP=`dig +short $DOMAIN`
 
-DOMAIN=`echo $ROW | awk '{split($0,a,"|"); print a[1]}'`
-OLDIP=`echo $ROW | awk '{split($0,a,"|"); print a[2]}'`
-NEWIP=`dig +short $DOMAIN`
+        if [ "$OLDIP" != "$NEWIP" ]; then
+                echo "$(date): Updating $DOMAIN"
+                if [ -n "$OLDIP" ]; then
+                        iptables -t nat -D PREROUTING -s $OLDIP/32 -i $IFACE -j ACCEPT -v && iptables-save > /etc/iptables/rules.v4 || iptables-save > /etc/iptables.rules
+                fi
+                iptables -t nat -I PREROUTING -s $NEWIP/32 -i $IFACE -j ACCEPT -v && iptables-save > /etc/iptables/rules.v4 || iptables-save > /etc/iptables.rules
 
-iptables -t nat -D PREROUTING -s $OLDIP/32 -i %s -j ACCEPT -v && iptables-save > /etc/iptables/rules.v4 || iptables-save > /etc/iptables.rules
-iptables -t nat -I PREROUTING -s $NEWIP/32 -i %s -j ACCEPT -v && iptables-save > /etc/iptables/rules.v4 || iptables-save > /etc/iptables.rules
-
-#UPDATE database
-sudo $(which sqlite3) ${SQLITE_DB} "UPDATE DDNS SET last_ipaddr = '${NEWIP}' WHERE domain = '${DOMAIN}'"
-
-
+                #UPDATE database
+                sudo $(which sqlite3) ${SQLITE_DB} "UPDATE DDNS SET last_ipaddr = '${NEWIP}' WHERE domain = '${DOMAIN}'"
+        fi
 done 
+
+exit 0
