aba8d69400ef597a93f98c4f6a7b8a30c302504e
Patrice Brend'amour <patrice@brendamour.net>
2017-01-28 19:11:24 +0100

    added schema update facility and first script for DDNS update

---
+++ auth/db/updates/0-to-2.5.sql
@@ -0,0 +1,11 @@
+BEGIN; 
+
+CREATE TABLE "DDNS"(
+	"ID" Integer PRIMARY KEY AUTOINCREMENT,
+	"created" DateTime DEFAULT CURRENT_TIMESTAMP,
+	"user_id" Integer NOT NULL,
+	"last_ipaddr" Text,
+	"domain" Text NOT NULL  );
+
+PRAGMA user_version = 2.5
+END; 
