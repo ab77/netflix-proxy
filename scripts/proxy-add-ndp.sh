fe52a03b657fe447e9ef3339ad134afff116e3a6
Anton Belodedenko <anton.belodedenko@gmail.com>
2017-03-30 08:41:17 +0200

    ndp stability

--- scripts/proxy-add-ndp.sh
+++ scripts/proxy-add-ndp.sh
@@ -3,10 +3,12 @@
 CDW=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 . ${CDW}/functions
 
-if [[ "${1}" == "-a" ]]; then
-    ndp_proxy_add_delete 'add'
-elif [[ "${1}" == "-d" ]]; then
-    ndp_proxy_add_delete 'delete'
-else
-    printf "usage\n\t${0} -a|-d\n"
+if [[ "${get_public6_prefix}" == "/64" ]]; then
+    if [[ "${1}" == "-a" ]]; then
+        ndp_proxy_add_delete 'add'
+    elif [[ "${1}" == "-d" ]]; then
+        ndp_proxy_add_delete 'delete'
+    else
+        printf "usage\n\t${0} -a|-d\n"
+    fi
 fi
