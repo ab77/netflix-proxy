7d535f62f533b5b02c7a98ea2b2bcf94df381248
belodetek <2033996+ab77@users.noreply.github.com>
2021-03-30 17:35:17 -0700

    upgrade pip

--- build.sh
+++ build.sh
@@ -299,6 +299,7 @@ sudo apt-get -y update &>> ${CWD}/netflix-proxy.log\
   && sudo apt-get -y install git python3.6 python3-venv python3-pip sqlite3 &>> ${CWD}/netflix-proxy.log\
   && python3 -m venv venv &>> ${CWD}/netflix-proxy.log\
   && source venv/bin/activate &>> ${CWD}/netflix-proxy.log\
+  && pip3 install pip --upgrade &>> ${CWD}/netflix-proxy.log\
   && pip3 install -r requirements.txt &>> ${CWD}/netflix-proxy.log\
   && pip3 install -r ${CWD}/auth/requirements.txt &>> ${CWD}/netflix-proxy.log
 log_action_end_msg $?
